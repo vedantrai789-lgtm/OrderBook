d8a01faf8610ed7ec0b80e41ba89a7477d99a98e
Vedant-rai <vedant.rai789@gmail.com>
2025-12-20 18:16:43 +0530

    created an echo server

---
+++ Server_main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+void check(int status, std::string msg)
+{
+    if (status < 0)
+    {
+        std::cerr << "Error: " << msg << " (" << strerror(errno) << ")\n";
+        exit(1);
+    }
+}
+int main()
+{
+    int port = 54321;
+
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    check(serverSocket, "Failed niga cuz u gay fam");
+
+    int opt = 1;
+    setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(port);
+
+    check(bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)), "Bind Failed");
+
+    check(listen(serverSocket, 10), "listen failed");
+    std::cout << "Server lsitening on port: " << port << "...\n";
+
+    sockaddr_in clientAddr;
+    socklen_t clientSize = sizeof(clientAddr);
+    int clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr, &clientSize);
+    check(clientSocket, "Accept Failed ");
+
+    std::cout << "Client connected mf!!\n";
+
+    char buffer[1024];
+    while (true)
+    {
+        // clear buffer first
+        memset(buffer, 0, 1024);
+
+        int bytesRead = read(clientSocket, buffer, 1024);
+        if (bytesRead <= 0)
+        {
+            std::cout << "Client disconnected.\n";
+            break;
+        }
+        std::cout << "Received: " << buffer << "\n";
+        std::string response = "server received: " + std::string(buffer);
+        write(clientSocket, response.c_str(), response.size());
+    }
+    close(clientSocket);
+    close(serverSocket);
+    return 0;
+}
